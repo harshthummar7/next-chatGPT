910d03413515607f46a14e63be4e1a49d39baa14
harshthummar7 <harsh.thummar@marutitech.com>
2023-05-30 18:27:52 +0530

    add drag and drop feature

--- components/FileUploader.js
+++ components/FileUploader.js
@@ -9,6 +9,7 @@ const FileUploader = () => {
   const [url, setUrl] = useState(null);
   const [load, setLoad] = useState(0);
   const [imgTag, setImgTag] = useState(false);
+  const [dragging, setDragging] = useState(true);
   const fileInputRef = useRef(null);
 
   const handleSelect = () => {
@@ -55,12 +56,30 @@ const FileUploader = () => {
         }
 
         setUrl(data.dataUrl);
+        setDragging(false);
       } else {
         console.error("Error uploading in file:", response.statusText);
       }
     } catch (error) {
       console.error("Error uploading  file:", error);
     }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDragEnter = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDragLeave = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setSelectedFile(event.dataTransfer.files[0]);
   };
 
   useEffect(() => {
@@ -81,16 +100,31 @@ const FileUploader = () => {
   return (
     <>
       <div className={style.main}>
-        <input
-          type="file"
-          ref={fileInputRef}
-          onChange={handleFileSelect}
-          style={{ display: "none" }}
-        />
-        <button onClick={handleSelect}>
-          <i className="bi bi-paperclip"></i>Select File
-          {selectedFile && <p>{selectedFile.name}</p>}
-        </button>
+        <div
+          className={style.dragging}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
+          <div className={style.first}>
+            <i className="bi bi-arrow-up-square-fill"></i>
+            <h3>Drag and Drop</h3>
+          </div>
+          <p>OR</p>
+          <div className={style.second}>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileSelect}
+              style={{ display: "none" }}
+            />
+            <button onClick={handleSelect}>
+              <i className="bi bi-paperclip"></i>Select File
+              {selectedFile && <p>{selectedFile.name}</p>}
+            </button>
+          </div>
+        </div>
 
         <button onClick={handleUpload} disabled={!selectedFile}>
           Upload
